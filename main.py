36f72bdf8dc285e086d5efc6e7befc537a19c6d1
maksimk5 <109814868+maksimkfive@users.noreply.github.com>
2023-08-14 18:11:34 +0400

    Update main.py
    v0.1.0:
    
    - Added a feature to display ping latency.
    - Enhanced the user interface to display information about the testing server.
    - Provided an option to save the speed test results to a file.
    - Removed the mode selection (single/multi) since it didn't affect the test.
    - Minor bug fixes and improvements.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import speedtest
+import datetime
 
 # ANSI color codes
 GREEN_TEXT = "\033[92m"
@@ -9,6 +10,7 @@ RESET_TEXT = "\033[0m"
 def print_green(text):
     """Prints the given text in green color."""
     print(f"{GREEN_TEXT}{text}{RESET_TEXT}")
+
 
 def print_red(text):
     print("\033[91m" + text + "\033[0m")
@@ -23,7 +25,15 @@ def test_speed():
     """Tests and prints the internet speed."""
     try:
         st = speedtest.Speedtest()
-        st.get_best_server()
+        best_server = st.get_best_server()
+
+        # Display server information
+        print_green(f"Testing using server: {best_server['sponsor']} ({best_server['name']}, {best_server['country']})")
+        print_green("--------------------------------------------------")
+
+        # Ping test (converted to integer)
+        ping_val = int(st.results.ping)
+        print_green(f"Ping: {ping_val} ms")
 
         print_green("Testing download speed...")
         download_speed = st.download() / 10 ** 6
@@ -32,35 +42,34 @@ def test_speed():
         print_green("Testing upload speed...")
         upload_speed = st.upload() / 10 ** 6
         print_green(f"Upload speed: {upload_speed:.2f} Mbps")
+
+        # Prompt to save results
+        save = input("Do you want to save the results? (y/n): ").strip().lower()
+        if save == 'y':
+            with open("speedtest_results.txt", "a") as f:
+                f.write(f"Date: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+                f.write(f"Server: {best_server['sponsor']} ({best_server['name']}, {best_server['country']})\n")
+                f.write(f"Ping: {ping_val} ms\n")
+                f.write(f"Download speed: {download_speed:.2f} Mbps\n")
+                f.write(f"Upload speed: {upload_speed:.2f} Mbps\n")
+                f.write("--------------------------------------------------\n")
+            print_green("Results saved!")
+
     except speedtest.ConfigRetrievalError as e:
         print_red(f"Error connecting to the server. {e}")
     except Exception as e:
         print_red(f"An unexpected error occurred. {e}")
 
 
-def get_user_choice():
-    """Prompts the user to choose a mode and returns the choice."""
-    print_green("Choose mode:")
-    print_green("1 - single (recommended)")
-    print_green("2 - multi")
-    return input().strip()
-
-
 def main():
     """Main function to run the speed test."""
     while True:
         clear_console()
-        mode = None
-
-        while mode not in ['1', '2']:
-            mode = get_user_choice()
-            if mode not in ['1', '2']:
-                print_green('Wrong mode selection! Try again.')
 
         test_speed()
 
         retry = input("Do you want to retry the speed test? (y/n): ").strip().lower()
-        if retry != 'y':
+        if retry == 'n':
             clear_console()
             break
 
